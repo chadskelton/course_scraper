50a9155200b55143d6f8ee1550df79edf1a1d446
Chad Skelton <cskeltonnews@gmail.com>
2019-06-25 10:35:36 -0700

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -15,14 +15,6 @@ import scraperwiki
 from datetime import datetime
 from BeautifulSoup import BeautifulSoup
 
-# trying this fix for 502 Bad Gateway Problem
-
-import ssl
-
-ssl._create_default_https_context = ssl._create_unverified_context
-
-# end of new fix
-
 br = mechanize.Browser()
 br.set_handle_robots(False)
 # br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
